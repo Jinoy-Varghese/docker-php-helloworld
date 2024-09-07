2e1380980158561cb69b4900bf21d24464945603
Jinoy <jinoy.v2000@gmail.com>
2024-09-07 23:18:25 +0530

    test

--- src/index.php
+++ src/index.php
@@ -6,4 +6,19 @@ for($i=1;$i<=5;$i++)
 {
   echo $i."<br>";
 }
+ParseClient::initialize( $app_id, $rest_key, $master_key);
+ParseClient::setServerURL('https://parseapi.back4app.com','/');
+
+$soccerPlayer = new ParseObject("SoccerPlayer");
+$soccerPlayer->set("name", "A. Wed");
+$soccerPlayer->set("yearOfBirth", 1997);
+$soccerPlayer->set('emailContact', 'a.wed@email.io');
+$soccerPlayer->setArray('attributes', ['fast', 'good conditioning']);
+
+try {
+  $soccerPlayer->save();
+  echo 'New object created with objectId: ' . $soccerPlayer->getObjectId();
+  } catch (ParseException $ex) {  
+  echo 'Failed to create new object, with error message: ' . $ex->getMessage();
+  }
 ?>
