a707002dc7a901b07c5753228b7ff3ba8bebb0c3
Jinoy <jinoy.v2000@gmail.com>
2024-09-08 14:09:40 +0530

    test

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '69ba4d83aea9dce07cd91876ac7336b9a74cf720',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '69ba4d83aea9dce07cd91876ac7336b9a74cf720',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'parse/php-sdk' => array(
+            'pretty_version' => '1.4.0',
+            'version' => '1.4.0.0',
+            'reference' => '19deeb473a33d9cc15271ef78f7520d95fa792ba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../parse/php-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
