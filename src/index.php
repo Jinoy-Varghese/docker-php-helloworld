9f316df7315f5ea8730c286bae35a16301651579
Jinoy <jinoy.v2000@gmail.com>
2024-09-08 14:14:50 +0530

    ygu

--- src/index.php
+++ src/index.php
@@ -21,3 +21,32 @@ for($i=1;$i<=5;$i++)
 //     print_r(json_encode($data, JSON_PRETTY_PRINT));
 //     curl_close($curl);
 ?>
+<script src="node_modules/parse/dist/parse.min.js"></script>
+
+<script>
+Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
+// Remember to inform BOTH the Back4App Application ID AND the JavaScript KEY
+Parse.initialize(
+  'qyAZuVFBpeAH6QGkz1IT5gK3eP5IZaphaFtZJMic', // This is your Application ID
+  'NDunEJ6qyg1AZ104lDqbC4969ItHhjXYoYXIwC1D' // This is your Javascript key
+);
+</script>
+<?php
+
+use Parse\ParseException;
+use Parse\ParseQuery;
+
+$query = new ParseQuery("MyCustomClassName");
+try {
+  $myCustomObject = $query->get("<PARSE_OBJECT_ID>");
+  // The object was retrieved successfully.
+
+  // To get attributes, you can use the "get" method, providing the attribute name:
+  $myCustomKey1Value = $myCustomObject->get("myCustomKey1Name");
+  echo $myCustomKey1Value;
+} catch (ParseException $ex) {
+  // The object was not retrieved successfully.
+  // error is a ParseException with an error code and message.
+}
+
+?>
